bec0bc6b7b822efe0526688f82aa251502defe06
Guy <gcschino@hotmail.com>
2025-12-07 11:28:39 +0000

    Day7 part 1

--- current/1.ts
+++ current/1.ts
@@ -1,32 +1,53 @@
-import { logAnswerCheck, sumInts } from "../utils.ts";
+import { logAnswerCheck } from "../utils.ts";
 
 const text = await Deno.readTextFile("current/input.txt");
 const data = text
   .trim()
   .split("\n")
-  .map((l) => l.split(/\s+/g).filter((i) => !!i));
+  .map((l) => l.split(""));
 
-const columnTotals = [] as number[];
+// For each line, each char,
+// if above is marked ('S') and this char is ., mark
+// if above is marked and this char is ^, add to splitTotal and mark left and right
+// if above is nor marked, ignore
 
-for (let i = 0; i < data[0].length; i++) {
-  const columnOperator = data.at(-1)?.at(i);
-  let total = columnOperator === "+" ? 0 : 1;
-  for (let rowIndex = 0; rowIndex < data.length - 1; rowIndex++) {
-    const value = parseInt(data[rowIndex][i]);
-    if (columnOperator === "*") {
-      total *= value;
-    } else {
-      total += value;
-    }
+let answer = 0;
+const processed = [] as string[][];
+
+data.forEach((line, index) => {
+  if (index === 0) {
+    processed.push(line);
+    return;
   }
-  columnTotals.push(total);
-}
 
-const answer = sumInts(columnTotals);
-console.log(data);
-console.log({ columnTotals });
+  const newLine = [...line];
+  // processed.push([]);
+  const lineAbove = processed.at(index - 1);
+  console.log(lineAbove);
+  line.forEach((char, charIndex) => {
+    const charAbove = lineAbove![charIndex];
+
+    if (charAbove === "S" && char === ".") {
+      newLine[charIndex] = "S";
+    } else if (charAbove === "S" && char === "^") {
+      answer ++;
+      newLine[charIndex] = ".";
+      if (charIndex > 0) {
+        newLine[charIndex - 1] = "S";
+      }
+      if (charIndex < newLine.length - 1) {
+        newLine[charIndex + 1] = "S";
+      }
+    }
+  });
+  processed.push(newLine);
+});
+
+console.log(data.map(l => l.join('')));
+console.log(processed.map(l => l.join('')));
+
 console.log("----");
 console.log(answer);
 console.log("----");
 
-logAnswerCheck(answer, 4277556);
+logAnswerCheck(answer, 21);
